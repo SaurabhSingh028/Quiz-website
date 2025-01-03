b1523c2706aae9184c60ef323c1c712c8ec48478
SaurabhSingh028 <85787780+SaurabhSingh028@users.noreply.github.com>
2025-01-03 16:21:24 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: "123456",
+  port: 5432,
+})
+
+db.connect();
 
 let quiz = [
   { country: "France", capital: "Paris" },
